dba8d419917206a05e622a95deb4160d2891490d
Adithya000 <86005781+Adithya000@users.noreply.github.com>
2022-11-24 00:52:25 +0530

    imageoption null

--- lib/studentprofile.dart
+++ lib/studentprofile.dart
@@ -22,60 +22,136 @@ class EditProfilePage2 extends StatefulWidget {
   _EditProfilePageState2 createState() => _EditProfilePageState2();
 }
 
-class _EditProfilePageState2 extends State<EditProfilePage2> {File? pickedImage;
-var nameEdit = TextEditingController();
-var classEdit = TextEditingController();
+class _EditProfilePageState2 extends State<EditProfilePage2> {
+  File? pickedImage;
+  var nameEdit = TextEditingController();
+  var classEdit = TextEditingController();
 
-Widget profileImage = Image.asset('assets/iconbg.png', fit: BoxFit.fill,);
+  Widget profileImage = Image.asset('assets/iconbg.png', fit: BoxFit.fill,);
 
-showAlertDialog(BuildContext context, TextEditingController _controller) {
-  Widget launchButton = TextButton(
-    onPressed: () {
-      Navigator.of(context).pop();
-    },
-    style: TextButton.styleFrom(
-      foregroundColor:  Color.fromARGB(
-          232, 18, 215, 241),
-    ),
-    child: Text(
-      'Ok',
-      style: TextStyle(fontSize: 15),
-    ),
-  );
 
-  // set up the AlertDialog
-  AlertDialog alert = AlertDialog(
-    actions: [
-      TextField(
-        controller: _controller,
+  showAlertDialog(BuildContext context, TextEditingController _controller) {
+    Widget launchButton = TextButton(
+      onPressed: () {
+        Navigator.of(context).pop();
+      },
+      style: TextButton.styleFrom(
+        foregroundColor:  Color.fromARGB(
+            232, 18, 215, 241),
       ),
-      launchButton,
-    ],
-  );
+      child: Text(
+        'Ok',
+        style: TextStyle(fontSize: 15),
+      ),
+    );
 
-  // show the dialog
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return alert;
-    },
-  );
-}
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      actions: [
+        TextField(
+          controller: _controller,
+        ),
+        launchButton,
+      ],
+    );
 
-pickImage(ImageSource imageType) async {
-  try {
-    final photo = await ImagePicker().pickImage(source: imageType);
-    if (photo == null) return;
-    final tempImage = File(photo.path);
-    setState(() {
-      pickedImage = tempImage;
-    });
-
-    Get.back();
-  } catch (error) {
-    debugPrint(error.toString());
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
   }
-}
+  final ImagePicker imgpicker = ImagePicker();
+  List<XFile>? imagefiles;
+
+  openImages() async {
+    try {
+      var pickedfiles = await imgpicker.pickMultiImage();
+      //you can use ImageCourse.camera for Camera capture
+      if(pickedfiles != null){
+        imagefiles = pickedfiles;
+        setState(() {
+        });
+      }else{
+        print("No image is selected.");
+      }
+    }catch (e) {
+      print("error while picking file.");
+    }
+  }
+  void imagePickerOption() {
+    Get.bottomSheet(
+      SingleChildScrollView(
+        child: ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(10.0),
+            topRight: Radius.circular(10.0),
+          ),
+          child: Container(
+            color: Colors.white,
+            height: 500,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const Text(
+                    "Pic Image From",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: () {pickImage(ImageSource.camera);
+
+                    },
+                    icon: const Icon(Icons.camera),
+                    label: const Text("CAMERA"),
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: () {pickImage(ImageSource.gallery);
+
+                    },
+                    icon: const Icon(Icons.image),
+                    label: const Text("GALLERY"),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      Get.back();
+                    },
+                    icon: const Icon(Icons.close),
+                    label: const Text("CANCEL"),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  pickImage(ImageSource imageType) async {
+    try {
+      final photo = await ImagePicker().pickImage(source: imageType);
+      if (photo == null) return;
+      final tempImage = File(photo.path);
+      setState(() {
+        pickedImage = tempImage;
+      });
+
+      Get.back();
+    } catch (error) {
+      debugPrint(error.toString());
+    }
+  }
 
 
   User user = UserPreferences.myUser;
@@ -103,6 +179,7 @@ pickImage(ImageSource imageType) async {
                   padding: EdgeInsets.all(10),
                   physics: BouncingScrollPhysics(),
                   children: [
+
                     /*CircleAvatar(
                       radius:MediaQuery.of(context).size.width*0.2,
                       backgroundColor: Colors.transparent,
@@ -115,14 +192,38 @@ pickImage(ImageSource imageType) async {
                     ),*/
                     Column(
                       children: [
+
                         CircleAvatar(
                           radius: MediaQuery.of(context).size.width*0.2,
                           backgroundColor: Colors.transparent,
-                          child: profileImage,
+                          child: pickedImage != null ? Image.file(
+                            pickedImage!,
+                            fit: BoxFit.fill,
+
+                          )
+                              : Image.asset('assets/iconbg.png', fit: BoxFit.fill,),
+
                         ),
-                        IconButton(onPressed: () {
-                        }, icon: Icon(Icons.add_a_photo_rounded, color: Color.fromARGB(
-                            232, 18, 215, 241),), iconSize: 25,),
+                        Positioned(
+                          bottom: 0,
+                          right: 5,
+                          child: IconButton(
+                            onPressed:imagePickerOption,
+                            icon: const Icon(
+                              Icons.add_a_photo_rounded,
+                              color: Color.fromARGB(
+                                  232, 18, 215, 241),
+                              size: 25,
+                            ),
+                          ),
+                        ),
+
+
+
+                        /* IconButton(onPressed: imagePickerOption, icon: Icon(
+
+                          Icons.add_a_photo_rounded, color: Color.fromARGB(
+                            232, 18, 215, 241),), iconSize: 25,),*/
                       ],
                     ),
 
@@ -140,10 +241,10 @@ pickImage(ImageSource imageType) async {
                             }, icon: Icon(Icons.edit), iconSize: 20,),
                           ],
                         ),
-                          style: ButtonStyle(
+                            style: ButtonStyle(
                               foregroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(
                                   232, 18, 215, 241),),
-                          ))
+                            ))
                       ],
                     ),
                     const SizedBox(height: 24),
